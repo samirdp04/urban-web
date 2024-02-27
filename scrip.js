af229d860b083cda41a7317c3a483c00f3ecbd50
Samir Del Portillo <151089691+samirdp04@users.noreply.github.com>
2024-02-27 16:28:44 -0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,109 @@
+const carrito = document.getElementById('carrito');
+const elementos1 = document.getElementById('lista-1');
+const lista = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+cargarEventListeners();
+
+function cargarEventListeners(){
+    elementos1.addEventListener('click', comprarElemento);
+    carrito.addEventListener('click', eliminarElemento);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+}
+
+function comprarElemento(e){
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemento(elemento);
+    }
+}
+
+function leerDatosElemento(elemento){
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('.precio').textContent,
+        id: elemento.querySelector('a').getAttribute('data-id')
+    }
+    insertarCarrito(infoElemento);
+}
+
+function insertarCarrito(elemento){
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${elemento.imagen}" width=100 >
+        </td>   
+        <td>
+            ${elemento.titulo}
+        </td>
+            ${elemento.precio}
+        <td>
+            <a href="#" class="borrar" data-id="${elemento.id}">X </a>
+        </td>
+    `;
+    lista.appendChild(row);
+}
+
+function eliminarElemento(e){
+    e.preventDefault();
+    let elemento,
+        elementoId;
+    if(e.target.classList.contains('borrar')) {
+        e.target.parentElement.parentElement.remove();
+        elemento = e.target.parentElement.parentElement;
+        elementoId = elemento.querySelector('a').getAttribute('data-id')
+    }   
+}
+
+function vaciarCarrito(){
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild);
+    }
+    return false;
+}
+
+// Formulario
+
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length < 3){
+        warnings += `El nombre no es válido  <br> <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es válido  <br> <br> `
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }
+})
+
+//  formulario select
+
+const optionMenu = document.querySelector(".select-menu"),
+    selectBtn = optionMenu.querySelector(".select-btn"),
+    options = optionMenu.querySelectorAll(".option"),
+    sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));    
+
+options.forEach(option =>{
+    option.addEventListener("click", () => {
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+        optionMenu.classList.remove("active");
+    })
+})    
